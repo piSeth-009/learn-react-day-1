43649579bd5901c9fa99e350879984fc0c20035f
piSeth-009 <menghak091234@gmail.com>
2024-08-28 17:59:48 +0700

    simple increment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-color": "#fffbf3",
+      },
+      margin: {
+        "350px": "350px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
